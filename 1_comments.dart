0dc52bc35ec7a1ad06cac528a5e7ed82b1e36989
narimetisaigopi <narimetisaigopi@gmail.com>
2020-03-08 00:00:49 +0530

    Create 1_comments.dart

---
+++ 1_comments.dart
@@ -0,0 +1,20 @@
+void main(){
+  
+  /* 
+   * multiline comments
+   * 
+   * sa
+   * 
+   */
+  
+  // printing hello world here
+  print("Hello World");
+  print("This is my first dart app"); 
+  print(987 / 99);
+  print(true); // printing boolean value
+}
+
+/// i am doing something doc com
+void doSomething(){
+  // somthing
+}
