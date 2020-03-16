708aa537c03eeae8195b830c0ec2673c5aff01c2
narimetisaigopi <narimetisaigopi@gmail.com>
2020-03-16 21:56:20 +0530

    Create 3_data_types.dart

---
+++ 3_data_types.dart
@@ -0,0 +1,30 @@
+void main(){
+
+  // numbers: int
+  int age = 10;
+  var age = 10;
+  
+  // numbers : double
+  double price = 10.50;
+  var price = 10.50;
+  
+  
+  // Strings
+  String name = "Sai Gopi";
+  String you = 'single quotes';
+  var you = 'single quotes'; 
+  
+  // boolean
+  
+  bool alive;
+  bool dead = false;
+  
+  
+  var alive =false;
+  
+  print(alive);
+  
+  
+  
+ 
+}
