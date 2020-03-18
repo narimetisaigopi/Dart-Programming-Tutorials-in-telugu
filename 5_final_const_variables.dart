afac9b307e005ea198bbc0f1023f5deab9b9ef34
narimetisaigopi <narimetisaigopi@gmail.com>
2020-03-18 23:08:27 +0530

    Create 5_final_const_variables.dart

---
+++ 5_final_const_variables.dart
@@ -0,0 +1,27 @@
+void main(){
+  
+ // final keyword
+  
+  final cityName = "Hyderabad";
+  
+  //cityName = "Delhi";
+  
+  final String myName = "Robin";
+  
+ // const keyword
+  
+  const countryName = 'india';
+  
+  const int age = 10;
+  
+  //countryName = 'canada';
+  
+  
+}
+
+class Circle{
+  
+  final color = "red";
+  
+  static const radius = 30;
+}
